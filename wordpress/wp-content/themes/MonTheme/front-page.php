7c6855158d526ee8cfac3acf082cb2dc11b70526
lorde78 <62546853+lorde78@users.noreply.github.com>
2022-03-09 23:42:07 +0100

    affichage taxo rubrique

--- wordpress/wp-content/themes/MonTheme/front-page.php
+++ wordpress/wp-content/themes/MonTheme/front-page.php
@@ -1,32 +1,72 @@
 <?php get_header(); ?>
 
-    <div class="taxonomie">
-        <h2>Catégories :</h2>
-        <ul>
-            <?php
-                $terms = get_terms(['taxonomy' => 'type-Logement']);
-                foreach($terms as $term) {
-                    $active = get_query_var('type-Logement') === $term->slug ? active : '';
-                    echo '<a class="linTerm '. $active .'" href="' . get_term_link($term) . '">' . $term->name . '</a><br>';
-                };
-            ?>
-        </ul>
-    </div>
+<?php
 
-    <div class="metaFiltre">
-        <h2>Systeme de filtres :</h2>
-        <ul>
-            <?php
-            ?>
-        </ul>
-    </div>
+$query_loft = new WP_Query([
+    'post_type' => 'Logement',
+    'tax_query' => [
+        [
+            'taxonomy' => 'type-Logement',
+            'field' => 'slug',
+            'terms' => 'loft'
+        ]
+    ],
+    'posts_per_page' => 1,
+]);
 
-	<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
-    
-    	<h1><?php the_title(); ?></h1>
-    
-    	<?php the_content(); ?>
+$query_appartement = new WP_Query([
+    'post_type' => 'Logement',
+    'tax_query' => [
+        [
+            'taxonomy' => 'type-Logement',
+            'field' => 'slug',
+            'terms' => 'a   ppartement'
+        ]
+    ],
+    'posts_per_page' => 1,
+    'orderby' => 'rand'
+]);
 
-	<?php endwhile; endif; ?>
+$query_villa = new WP_Query([
+    'post_type' => 'Logement',
+    'tax_query' => [
+        [
+            'taxonomy' => 'type-Logement',
+            'field' => 'slug',
+            'terms' => 'villa'
+        ]
+    ],
+    'posts_per_page' => 1,
+    'orderby' => 'rand'
+]);
+?>
+
+<?php if ($query_loft->have_posts()) : ?>
+    <?php
+    while ($query_loft->have_posts()) : $query_loft->the_post();
+    ?>
+        <?php get_template_part('partials/post-render'); ?>
+<?php endwhile;
+endif;
+wp_reset_postdata();
+?>
+
+<?php if ($query_appartement->have_posts()) : ?>
+    <?php
+    while ($query_appartement->have_posts()) : $query_appartement->the_post();
+    ?>
+        <?php get_template_part('partials/post-render'); ?>
+<?php endwhile;
+endif;
+wp_reset_postdata(); ?>
+
+<?php if ($query_villa->have_posts()) : ?>
+    <?php
+    while ($query_villa->have_posts()) : $query_villa->the_post();
+    ?>
+        <?php get_template_part('partials/post-render'); ?>
+<?php endwhile;
+endif;
+wp_reset_postdata(); ?>
 
 <?php get_footer(); ?>
