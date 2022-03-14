8305d5b4822acb8c192fb1f26cc6c5a4f73b1ced
bentaniamin1 <amin.bentani@hetic.net>
2022-03-14 09:49:59 +0100

    branche login

--- wordpress/wp-content/themes/MonTheme/templates/template-inscription.php
+++ wordpress/wp-content/themes/MonTheme/templates/template-inscription.php
@@ -10,25 +10,32 @@ get_header();
 if (is_user_logged_in()) {
     wp_redirect(home_url());
 } else { ?>
-    <form action="<?= admin_url('admin-post.php?action=inscription'); ?>" name="inscriptionform" method="POST">
-        <div>
-            <label for="inputIdentifiant" class="form-label"> Identifiant</label>
-            <input type="text" class="form-control" name="identifiant" id="inputIdentifiant">
+    <div class="container-inscription">
+        <div class="sideLeft">
+            <div class="img_container">
+                <img src="<?= get_template_directory_uri() . '/images/OIP.jpg'  ?>" alt="">
+                <div class="overlay"></div>
+            </div>
         </div>
-        <div>
-            <label for="inputEmail" class="form-label"> Email</label>
-            <input type="text" class="form-control" name="email" id="inputEmail">
+        <div class="sideRight">
+            <form action="<?= admin_url('admin-post.php?action=inscription'); ?>" name="inscriptionform" method="POST">
+                <label for="inputIdentifiant" class="form-label"> Identifiant</label>
+                <input type="text" class="form-control" name="identifiant" id="inputIdentifiant">
+
+                <label for="inputEmail" class="form-label"> Email</label>
+                <input type="text" class="form-control" name="email" id="inputEmail">
+
+                <label for="inputPassword" class="form-label"> Password</label>
+                <input type="password" class="form-control" name="password" id="inputPassword">
+
+                <input type="hidden" name="action" value="inscription_form">
+
+                <input type="submit" class="submitButton" name="submit" value="Se connecter">
+                <input type="hidden" name="task" value="register" />
+
+            </form>
         </div>
-        <div>
-            <label for="inputPassword" class="form-label"> Password</label>
-            <input type="password" class="form-control" name="password" id="inputPassword">
-        </div>
-        <input type="hidden" name="action" value="inscription_form">
 
-        <input type="submit" class="submitButton" name="submit" value="Se connecter">
-        <input type="hidden" name="task" value="register" />
-
-    </form>
-
+    </div>
 <?php  }
 get_footer(); ?>
