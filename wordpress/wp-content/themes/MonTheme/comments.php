dfe55df732d3bef080a7742196ff47233acbdccc
lorde78 <62546853+lorde78@users.noreply.github.com>
2022-03-14 14:37:59 +0100

    push

--- wordpress/wp-content/themes/MonTheme/comments.php
+++ wordpress/wp-content/themes/MonTheme/comments.php
@@ -4,3 +4,5 @@ comment_form();
 if (is_singular() && comments_open() && (get_option('thread_comments') == 1)) {
     wp_enqueue_script('comment-reply', 'wp-includes/js/comment-reply', array(), false, true);
 }
+
+// ajouter pagination pour les commentaires
--- wordpress/wp-content/themes/MonTheme/single-logement.php
+++ wordpress/wp-content/themes/MonTheme/single-logement.php
@@ -58,27 +58,10 @@ $query = new WP_Query([
     <div class="post_similaire_container">
       <?php
       while ($query->have_posts()) : $query->the_post();
-      ?>
-        <article class="post">
-          <?php the_post_thumbnail(); ?>
-
-          <h1><?php the_title(); ?></h1>
-          <?php the_permalink() ?>
-
-          <div class="post__meta">
-            <p>
-              Publié le <?php the_date(); ?>
-              par <?php the_author(); ?>
-              Dans la catégorie <?php the_category(); ?>
-              Avec les étiquettes <?php the_tags(); ?>
-            </p>
-            <?php the_terms(get_the_ID(), 'type-Logement'); ?>
-          </div>
-
-          <div class="post__content">
-            <?php the_content(); ?>
-          </div>
-        </article>
+      
+        get_template_part('partials/post-render');
+        
+    ?>
     <?php endwhile;
     endif;
     wp_reset_postdata(); ?>
