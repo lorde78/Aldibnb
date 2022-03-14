f57961f5423e8e8e777b35bbd91d04bc74c20ac6
bentaniamin1 <amin.bentani@hetic.net>
2022-03-14 10:12:13 +0100

    ajout total

--- wordpress/wp-content/themes/MonTheme/single-logement.php
+++ wordpress/wp-content/themes/MonTheme/single-logement.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <?php if (have_posts()) : while (have_posts()) : the_post();
-    get_template_part('partials/post-render');
+    get_template_part('partials/post-single-render');
   endwhile;
 endif; ?>
 
